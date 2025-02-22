c9b5d4832486ff2dd42f661e9e0214315d3fb6ef
Dmitriy Filippov <dmitriy.filippov.reid@gmail.com>
2025-02-22 16:57:59 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types;
+using MyTelegramBot.secure;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+
+namespace HomeWork24
+{
+    internal class Program
+    {
+        private static MyAppConfig appConfig = new MyAppConfig();
+
+        private static void OnHandleUpdateStarted(string message)
+        {
+            Console.WriteLine($"Началась обработка сообщения '{message}'");
+        }
+
+        private static void OnHandleUpdateCompleted(string message)
+        {
+            Console.WriteLine($"Закончилась обработка сообщения '{message}'");
+        }
+
+        static async Task Main()
+        {
+            if (!appConfig.ReadConfig())
+            {
+                return;
+            }
+
+            var cts = new CancellationTokenSource();
+            var botClient = new TelegramBotClient(appConfig.telegramApiKey);
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = [UpdateType.Message],
+                DropPendingUpdates = true
+            };
+            var handler = new UpdateHandler();
+            handler.OnHandleUpdateStarted += OnHandleUpdateStarted;
+            handler.OnHandleUpdateCompleted += OnHandleUpdateCompleted;
+            try 
+            {
+                botClient.StartReceiving(handler, receiverOptions, cts.Token);
+
+                var me = await botClient.GetMe();
+                Console.WriteLine($"{me.FirstName} запущен!");
+
+                string a = "";
+                while (true)
+                {
+                    Console.WriteLine("Нажмите клавишу A для выхода");
+                    a = Console.ReadLine();
+                    if (a == "A")
+                    {
+                        await cts.CancelAsync(); 
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{me.Id} работает!");
+                    }
+                }
+                //await Task.Delay(-1); // Устанавливаем бесконечную задержку, чтобы наш бот работал постоянно
+            }
+            finally
+            {
+                handler.OnHandleUpdateStarted -= OnHandleUpdateStarted;
+                handler.OnHandleUpdateCompleted -= OnHandleUpdateCompleted;
+            }
+
+        }
+
+    }
+}
---
+++ UpdateHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using static Telegram.Bot.TelegramBotClient;
+
+namespace HomeWork24
+{
+    public delegate void MessageHandler(string message);
+    record CatFactDto(string Fact, int length);
+
+    internal class UpdateHandler : IUpdateHandler
+    {
+        public MessageHandler? OnHandleUpdateStarted;
+        public MessageHandler? OnHandleUpdateCompleted;
+        //public record CatFactDto(string Fact, int length);
+
+        async Task IUpdateHandler.HandleErrorAsync(ITelegramBotClient botClient, Exception exception, HandleErrorSource source, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        async Task IUpdateHandler.HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            OnHandleUpdateStarted?.Invoke(update.Message.Text);
+            _=botClient.SendMessage(update.Message.Chat.Id, "Сообщение успешно принято", cancellationToken: token);
+
+            if (update.Message.Text == "/cat")
+            {
+                var cts = new CancellationTokenSource();
+                using var client = new HttpClient();
+                object? catFact_ = await client.GetFromJsonAsync<CatFactDto>("https://catfact.ninja/fact", cts.Token);
+                if (catFact_ is CatFactDto catFact)
+                {
+                    _=botClient.SendMessage(update.Message.Chat.Id, $"Fact: {catFact.Fact}   length: {catFact.length}", cancellationToken: token);
+                }
+                await cts.CancelAsync();
+            }
+            OnHandleUpdateCompleted?.Invoke(update.Message.Text);
+        }
+    }
+}
